cd007838a4f23fd5e7d8e3215d83b9e34fae1ee1
sakibit17043 <it17043@mbstu.ac.bd>
2024-09-04 14:20:05 +0600

    vowel-consonent-digit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Test{
+    public static void Main(string[]args){
+      string text = "Hello!This is 123  Sakib.";
+      int vowel = 0;
+      int consonent = 0;
+      int digit =0;
+      foreach(char character in text.ToLower()){
+        if(character=='a'||character=='i'||character=='e'||character=='o'||character=='u'){
+          vowel++;
+        }
+        else if(character>='a'&&character<='z'){
+          consonent++;
+        }
+        else if(char.IsDigit(character)){
+          digit++;
+        }
+      }
+      Console.WriteLine($"vowel:{vowel}\nconsonent:{consonent}\ndigit:{digit}");
+    }   
+    }
+
